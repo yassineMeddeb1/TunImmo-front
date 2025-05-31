b6fcd59d742bc90f77fe8e8d5a4738b7dc7ff7af
yassine <meddeby42@gmail.com>
2025-05-31 22:19:59 +0200

    commit avec render

--- src/environements/environment.ts
+++ src/environements/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     apiUrl: 'https://tuniimmo-back.onrender.com/api',
+    apiUrlCat: 'https://tuniimmo-back.onrender.com',
     stripePublishableKey: 'pk_test_51RIILgQOBevw5P2MAEwAhqVFTvEfy1D9xghqEUBRHx3tvQannAcKwaDsklPxgpchNvedsLnMNkaHvcTx7vbMPgow00Ox43QHNC'
 };
   
